37616ac9f99be018eac98afb565aa773203ed425
IgnacioMagurno <ignaciomagurno02@gmail.com>
2023-06-04 01:41:23 -0300

    0.7

--- client/src/pages/InfoCompletaAlumno.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { AlumnoInfoCompleta } from "../components/AlumnoInfoCompleta";
-
-export default function InfoCompletaAlumno() {
-  return <AlumnoInfoCompleta />;
-}
