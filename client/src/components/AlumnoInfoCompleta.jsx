0b48fb5f51112b0947d2299988d33ec4a029c187
IgnacioMagurno <ignaciomagurno02@gmail.com>
2023-06-09 14:58:31 -0300

    AlumnoInfoCompleta-Design

--- client/src/components/AlumnoInfoCompleta.jsx
+++ client/src/components/AlumnoInfoCompleta.jsx
@@ -18,61 +18,61 @@ export function AlumnoInfoCompleta() {
   return (
     <>
       <div className="flex justify-center items-center bg-full-bg-color">
-        <div className="flex flex-col items-center sm:hidden max-h-[90%] max-w-[65%] p-14 m-14 bg-white w-full h-full box-border shadow-xl">
-          <div className="absolute top-20 left-60">
+        <div className="flex flex-col items-center xl:max-h-[90%] xl:max-w-[65%] sm:max-w-[100%] sm:max-h-[100%] p-14 m-14 bg-white w-full h-full box-border shadow-xl">
+          <div className="absolute top-32 left-52">
             <BackButton />
           </div>
           {alumno && (
-            <div className="flex flex-col gap-2 p-10 border border-black">
+            <div className="flex flex-col gap-2 p-10 border border-black ">
               <div className="">
                 <h1 className="text-center font-bold text-3xl mb-5">
                   Informacion completa de {alumno.Fullname}{" "}
                 </h1>
               </div>
               <div className="flex flex-row border border-black">
-                <label className="border p-2 text-center w-1/6 border-r-black">
+                <label className="border p-2 text-center w-2/6 border-r-black">
                   Nombre
                 </label>
                 <h1 className="p-2 w-1/6 ">{alumno.Fullname}</h1>
               </div>
               <div className="flex flex-row border border-black">
-                <label className="border text-center p-2 w-1/6 border-r-black">
+                <label className="border text-center p-2 w-2/6 border-r-black">
                   Escuela
                 </label>
                 <p className="p-2 w-1/6">{alumno.Escuela}</p>
               </div>
               <div className="flex flex-row border border-black">
-                <label className="border text-center p-2 w-1/6 border-r-black">
+                <label className="border text-center p-2 w-2/6 border-r-black">
                   Fecha de Nacimiento
                 </label>
-                <p className="p-2 w-1/6">{alumno.FechaDeNacimiento}</p>
+                <p className="p-2 w-2/6">{alumno.FechaDeNacimiento}</p>
               </div>
               <div className="flex flex-row border border-black">
-                <label className="border text-center p-2 w-1/6 border-r-black">
+                <label className="border text-center p-2 w-2/6 border-r-black">
                   DNI
                 </label>
                 <p className="p-2 w-1/6">{alumno.DNI}</p>
               </div>
               <div className="flex flex-row border border-black">
-                <label className="border text-center p-2 w-1/6 border-r-black">
+                <label className="border text-center p-2 w-2/6 border-r-black">
                   Legajo
                 </label>
                 <p className="p-2 w-1/6">{alumno.Legajo}</p>
               </div>
               <div className="flex flex-row border border-black">
-                <label className="border text-center p-2 w-1/6 border-r-black">
+                <label className="border text-center p-2 w-2/6 border-r-black">
                   Telefono
                 </label>
                 <p className="p-2 w-1/6">{alumno.telefono}</p>
               </div>
               <div className="flex flex-row border border-black">
-                <label className="border text-center p-2 w-1/6 border-r-black">
+                <label className="border text-center p-2 w-2/6 border-r-black">
                   Curso
                 </label>
                 <p className="p-2 w-1/6">{alumno.Curso}</p>
               </div>
               <div className="flex flex-row border border-black">
-                <label className="border text-center p-2 w-1/6 border-r-black">
+                <label className="border text-center p-2 w-2/6 border-r-black">
                   Turno
                 </label>
                 <p className="p-2 w-1/6">{alumno.Turno}</p>
